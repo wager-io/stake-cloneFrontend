154cce7503209e2a265329d4c1b3e0156a33722f
Highscoretech <highscoretechng@gmail.com>
2025-06-28 08:55:23 +0100

    reset crash game bg

--- src/Games/Crash/Index.jsx
+++ src/Games/Crash/Index.jsx
@@ -31,8 +31,8 @@ const CrashGameContent = () => {
 
   return (
     <div className="w-full max-w-7xl mx-auto p-2 pb-20 md:pb-10 md:p-5 text-white font-sans"> 
-      <div className="bg-[var(--card-bg-10)] bg-opacity-80 rounded-[18px] shadow-lg shadow-gray-900/50 p-1 md:p-5 mb-5">
-        <div className="flex flex-col-reverse md:flex-row gap-5">
+      <div className="bg-gray-900 bg-opacity-80 rounded-[18px] shadow-lg shadow-gray-900/50 p-0 md:p-5 mb-5">
+        <div className="flex flex-col-reverse md:flex-row gap-3">
           <CrashControls />
           <CrashCanvas />
         </div>
